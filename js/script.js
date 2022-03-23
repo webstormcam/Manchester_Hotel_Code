5b57cd80cd2546e013cc2a282704d0991471df02
Cameron Chandler <webstormcam@gmail.com>
2022-03-23 09:20:52 -0400

    Working on the side menu on mobile

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("Sidenav").style.width = "250px";
+    console.log('Hello')
+  }
+  
+  function closeNav() {
+    document.getElementById("Sidenav").style.width = "0";
+  }
