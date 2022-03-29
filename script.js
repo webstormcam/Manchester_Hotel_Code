8d2a0a829d1ed3c02b90d8a83e918f4ef32fbf88
Cameron Chandler <webstormcam@gmail.com>
2022-03-29 18:22:54 -0400

    Starting over with the story gallery

--- script.js
+++ script.js
@@ -1,34 +1,11 @@
 function openNav() {
     document.getElementById("Sidenav").style.width = "250px";
-    console.log('Hello')
+   
   }
   
   function closeNav() {
     document.getElementById("Sidenav").style.width = "0";
   }
-
-
-  // Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var gall = document.getElementById("gallery");
-var modalImg = document.getElementById("gallery-photo");
-var captionText = document.getElementById("caption");
-gall.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = "imgs/The_Manchester_Hotel_Photo.png";
-  captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
 
 let slideIndex = 1;
 showSlides(slideIndex);
