a7a432b8057f5110a36226d6598738ec8901d01e
Cameron Chandler <webstormcam@gmail.com>
2022-03-29 13:18:45 -0400

    Starting to build the model

--- js/script.js
+++ js/script.js
@@ -6,3 +6,25 @@ function openNav() {
   function closeNav() {
     document.getElementById("Sidenav").style.width = "0";
   }
+
+
+  // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var gall = document.getElementById("gallery");
+var modalImg = document.getElementById("gallery-photo");
+var captionText = document.getElementById("caption");
+gall.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
